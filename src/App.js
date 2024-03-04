90ea0cb09498782811ef9e5f7114dcbf2ef5becf
Sk Mijanur Rahaman <mijansk1994@gmail.com>
2024-03-04 13:03:38 +0530

    Upto 3rd folder_image with Bulma & CSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 // import BrightText from "./ProfileCard";
-import AlexaImage from "./image/alexa.png"
-import CortanaImage from "./image/cortana.png"
-import SiriImage from "./image/siri.png"
+import AlexaImage from "./image/alexa.png";
+import CortanaImage from "./image/cortana.png";
+import SiriImage from "./image/siri.png";
 
-
-
-    
-function App(){
-
-    return(
-        <div>
-            <div><h3>Personal Digital Assistants</h3></div>
-
-           <ProfileCard title="Alexa" handle="@alexa.help" imageSrc={AlexaImage} />
-           <ProfileCard title="Cortana" handle="@cortana222.help" imageSrc={CortanaImage}/>
-           <ProfileCard title="Siri" handle="@siri07.help" imageSrc={SiriImage}/>
+/**
+ * The App function is the main function of this application. It renders a
+ * simple header and three BrightText components, each with a different color.
+ 
+ *
+ *
+ * @return A brighttext component
+ *
+ * @docauthor Trelent
+ */
+function App() {
+  return (
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
         </div>
-    )
-    // return(
-    //     <div>
-    //         <BrightText color="red"/>
-    //         <BrightText color="green"/>
-    //         <BrightText color="blue"/>
-    //     </div>
-    // )
-};
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa.help"
+                imageSrc={AlexaImage}
+                description="Alexa is created by Amazona and helps you make life easier!"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana222.help"
+                imageSrc={CortanaImage}
+                description="Cortana is made by Microsoft. It is very helpsful in different ways!"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri07.help"
+                imageSrc={SiriImage}
+                description="Siri is created by Apple and helps you to access every virtual things easily."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+  // return(
+  //     <div>
+  //         <BrightText color="red"/>
+  //         <BrightText color="green"/>
+  //         <BrightText color="blue"/>
+  //     </div>
+  // )
+}
 export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,18 +1,26 @@
-function ProfileCard({title, handle, imageSrc}) {
-    //const {title, handle} = props;
-   
-    return (
-    
-    <div>
-        <div>Title is {title}</div>
-        <div>Handle is {handle}</div>
-        <img src= {imageSrc}/ >
+function ProfileCard({ title, handle, imageSrc, description }) {
+  //const {title, handle} = props;
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is 1by1">
+          <img src={imageSrc} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
-    )
-};
+  );
+}
 // function BrightText({color}){
 //     const style ={color:color}
-//     return<h1 style={style}> Ka Ka Chi CHi!!</h1>
+//     return<h1 style={style}> Hello!!!</h1>
 // }
 
 export default ProfileCard;
